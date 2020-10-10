82f2b30cb7b6f2aac6dda2398b1ae003bd499a09
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-10 17:01:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,17 @@
 class Application
 
-  def call(env)
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   resp.write "Hello, World"
+  #   resp.finish
+  # end
+
+
+def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    
+    
     resp.finish
   end
-
+  
 end
